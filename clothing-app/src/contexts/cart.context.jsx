d6c850c0f64c4e9de95a9a3efee7836419d95c5b
Antonio Takev <tonitakev@gmail.com>
2022-07-12 20:17:28 +0200

    Add reducers to user and cart contexts as replacement of useState

--- clothing-app/src/contexts/cart.context.jsx
+++ clothing-app/src/contexts/cart.context.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useReducer } from "react";
+import { createAction } from "../utils/reducer/reducer.utils";
 
 const addCartItem = (cartItems, productToAdd) => {
     const existingcartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id)
@@ -37,33 +38,70 @@ export const CartContext = createContext({
     cartTotal: 0,
 })
 
+const INITIAL_STATE = {
+    isCartOpen: false,
+    cartItems: [],
+    cartCount: 0,
+    cartTotal: 0
+}
+
+const CART_ACTION_TYPES = {
+    'SET_IS_CART_OPEN': 'SET_IS_CART_OPEN',
+    'SET_CART_ITEMS': 'SET_CART_ITEMS'
+}
+
+const cartReducer = (state, action) => {
+    const { type, payload } = action
+
+    switch(type) {
+        case CART_ACTION_TYPES.SET_CART_ITEMS:
+            return {
+                ...state,
+                ...payload
+            }
+        case CART_ACTION_TYPES.SET_CART_COUNT:
+            return {
+                ...state,
+                isCartOpen: payload
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in cartReducer`)
+    }
+}
+
 export const CartProvider = ({children}) => {
-    const [isCartOpen, setIsCartOpen] = useState(false);
-    const [cartItems, setCartItems] = useState([]);
-    const [cartCount, setCartCount] = useState(0);
-    const [cartTotal, setCartTotal] = useState(0);
 
-    useEffect(() => {
-        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
-        setCartCount(newCartCount)
-    }, [cartItems])
+    const [state, dispatch] = useReducer(cartReducer, INITIAL_STATE)
+    const { isCartOpen, cartItems, cartCount, cartTotal } = state
 
-    useEffect(() => {
+    const updateCartItems = (newCartItems) => {
+        const newCartCount = cartItems.reduce(
+            (total, cartItem) => total + cartItem.quantity, 0)
         const newCartTotal = cartItems.reduce(
             (total, cartItem) => total + cartItem.quantity * cartItem.price, 0)
-        setCartTotal(newCartTotal)
-    }, [cartItems])
+        
+        const payload = {
+            cartItems: newCartItems,
+            cartCount: newCartCount,
+            cartTotal: newCartTotal
+        } 
+        dispatch(createAction(CART_ACTION_TYPES.SET_CART_ITEMS, payload))
+    }
+
+    const setIsCartOpen = (isCartOpen) => {
+        dispatch(createAction(CART_ACTION_TYPES.SET_CART_COUNT,isCartOpen))
+    }
 
     const addItemToCart = (productToAdd) => {
-        setCartItems(addCartItem(cartItems, productToAdd))
+        updateCartItems(addCartItem(cartItems, productToAdd))
     }
 
     const removeItemFromCart = (cartItemToRemove) => {
-        setCartItems(removeCartItem(cartItems, cartItemToRemove))
+        updateCartItems(removeCartItem(cartItems, cartItemToRemove))
     }
 
     const clearItemFromCart = (cartItemToClear) => {
-        setCartItems(clearCartItem(cartItems, cartItemToClear))
+        updateCartItems(clearCartItem(cartItems, cartItemToClear))
     }
 
     const value = { isCartOpen, setIsCartOpen, addItemToCart, removeItemFromCart, clearItemFromCart, cartItems, cartCount, cartTotal };
--- clothing-app/src/contexts/user.context.jsx
+++ clothing-app/src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useEffect, useReducer } from 'react'
+import { createAction } from '../utils/reducer/reducer.utils';
 
 import { onAuthStateChangedListener, createUserDocumentfromAuth } from '../utils/firebase/firebase.utils'
 
@@ -8,9 +9,40 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+    'SET_CURRENT_USER': 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+    console.log("dispatch")
+    console.log(action)
+    const { type, payload } = action
+    
+
+    switch(type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`)
+    }
+
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 // the component
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null)
+    const [ state, dispatch ] = useReducer(userReducer, INITIAL_STATE)
+    const { currentUser } = state
+    console.log(currentUser)
+    const setCurrentUser = (user) => {
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user))
+    }
     const value = { currentUser, setCurrentUser }
 
 
